6fbca6ffe0e9c82763243beb26dd2f28cb402da8
Valentin Roecklinger <v.roecklinger@palfinger.com>
2023-03-11 21:13:30 +0100

    feat(FM): style adaptations

---
+++ src/app/styles/variables/spacing.ts
@@ -0,0 +1,12 @@
+// KEEP THIS FILE IN SYNC WITH spacing.scss
+export interface DsSizes {
+  spacing: number;
+  borderRadius: number;
+  iconSize: number;
+}
+
+export const sizes: DsSizes = {
+  spacing: 20,
+  borderRadius: 2,
+  iconSize: 24,
+};
